ec136a5b89d72685b9d76a303dc887e6a5e440ca
pcpulabfed <pcpulabfed@localhost.localdomain>
2016-04-20 18:02:49 +0800

    2d draw

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,8 @@ extern SDL_Renderer*		mainRenderer;
 extern SDL_Texture*		    defaultTexture;
 uint32_t*			        defaultBuffer = NULL;
 int				            defaultLpitchByte;
+uint32_t                    frames;
+uint32_t                    lastTime, currentTime;
 extern int32_t			            mainWindowHeight;
 extern int32_t			            mainWindowWidth;
 ARDA_RESULT GameInit(void);
@@ -45,9 +47,9 @@ ARDA_RESULT GameInit(void){
     ARDA_SetClipRect(cr);
     triangle = {1, 3, 300, 300, 0, 0, _RGB32(255, 0, 127, 127), NULL};
     triangle.vlist = (VERTEX2DF*)malloc(triangle.num_verts*sizeof(VERTEX2DF));
-    triangle.vlist[0] = {-120, 20};
-    triangle.vlist[1] = {20, 400};
-    triangle.vlist[2] = {10, 40};
+    triangle.vlist[0] = {-20, 400};
+    triangle.vlist[1] = {700, 900};
+    triangle.vlist[2] = {300, 160};
     return flag;
 }
 ARDA_RESULT GameShutdown(void){
@@ -55,15 +57,21 @@ ARDA_RESULT GameShutdown(void){
 }
 ARDA_RESULT GameMain(void){
     static int32_t lpitch;
+    currentTime = SDL_GetTicks();
+    frames ++;
+    if(currentTime - lastTime > 1000){
+        SDL_Log("%f frames", (float) frames / (currentTime - lastTime) * 1000);
+        lastTime = currentTime;
+        frames = 0;
+    }
     SDL_LockTexture(defaultTexture, NULL, (void**)&defaultBuffer, &defaultLpitchByte);
     lpitch = defaultLpitchByte >> 2;
     memset((void*)defaultBuffer, 0 , mainWindowHeight * defaultLpitchByte);
-    ARDA_VLine(-10, 2000, 500, _RGB32(255, 127, 127, 0), defaultBuffer, lpitch);
-    ARDA_HLine(-10, 2000, 500, _RGB32(255, 127, 127, 0), defaultBuffer, lpitch);
     ARDA_TranslatePolygon2D(&triangle, 1, 1);
     ARDA_RotatePolygon2D(&triangle, -362);
-    ARDA_ScalePolygon2D(&triangle, 0.98, 1.02);
+    ARDA_ScalePolygon2D(&triangle, 1.01, 1.01);
     ARDA_DrawPolygon2D(&triangle, defaultBuffer, lpitch);
+    ARDA_FilledTriangle2D(triangle.vlist[0].x + triangle.x0, triangle.vlist[0].y + triangle.y0, triangle.vlist[1].x + triangle.x0, triangle.vlist[1].y + triangle.y0, triangle.vlist[2].x + triangle.x0, triangle.vlist[2].y + triangle.y0, _RGB32(255,127,0,0),defaultBuffer,lpitch);
     SDL_UnlockTexture(defaultTexture);
     SDL_RenderClear(mainRenderer);
     SDL_RenderCopy(mainRenderer, defaultTexture, NULL, NULL);
